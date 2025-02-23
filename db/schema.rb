d96158edf4b0873e86ebdbd3f4d56c2d64d6b87b
kannna5296 <kannnahacks5296@gmail.com>
2025-02-23 18:00:19 +0900

    micropostsのscafolldとdbmigrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_23_084709) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_23_085951) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
